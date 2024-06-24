9c0d35e5a1cbfefc6c0ccc9c61a9319758ea8f74
matheus <matheuscosta.tech@gmail.com>
2024-06-23 22:32:49 -0300

    Highlighted projects section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,32 @@
-import { Button, Image } from "@nextui-org/react";
+import { Button, Card, CardBody, CardHeader, Image } from "@nextui-org/react";
+import { ArrowRight } from "lucide-react";
 
 export default function Home() {
+  const highlightedProjects = [
+    {
+      name: "Barbershop",
+      imgSrc: "images/projects/barbershop1.png",
+      description:
+        "A barbershop website where the user can book an appointment and see a real time queue",
+      date: "Oct 2021",
+    },
+    {
+      name: "Birthday Reminder",
+      imgSrc: "images/projects/birthday-reminder1.png",
+      description: "A simple app to remind birthdays via push notifications",
+      date: "May 2024",
+    },
+    {
+      name: "Barbershop",
+      imgSrc: "images/projects/barbershop1.png",
+      description:
+        "A barbershop website where the user can book an appointment and see a real time queue",
+      date: "Oct 2021",
+    },
+  ];
+
   return (
-    <main className="flex items-center justify-center max-w-[1024px] mx-auto">
+    <main className="max-w-[1024px] mx-auto">
       <section className="hero-section">
         <div className="hero-main-text">
           <em className="text-2xl">
@@ -108,6 +132,39 @@ export default function Home() {
           alt="Hero image"
           src="images/hero-img.png"
         />
+      </section>
+
+      <section className="highlighted-project-section">
+        <h1 className="text-xl mb-4 font-bold">
+          Take a look at my highlighted projects
+        </h1>
+
+        <div className="flex gap-4 mb-4">
+          {highlightedProjects.map((p, i) => (
+            <Card key={i} className="p-2">
+              <CardHeader className="flex-col items-start">
+                <h4 className="font-bold text-large">{p.name}</h4>
+                <small className="text-default-500">{p.date}</small>
+                <p className="text-sm">{p.description}</p>
+              </CardHeader>
+              <CardBody className="overflow-visible">
+                <Image
+                  alt="Card image"
+                  src={p.imgSrc}
+                  height={350}
+                  className="object-cover"
+                />
+              </CardBody>
+            </Card>
+          ))}
+        </div>
+
+        <div className="flex justify-center">
+          <Button className="bg-black text-white">
+            See all
+            <ArrowRight />
+          </Button>
+        </div>
       </section>
     </main>
   );
