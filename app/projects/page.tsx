67e1d93cb7cb3cc6596e8c8b92a7c0ff0beec92e
matheus <matheuscosta.tech@gmail.com>
2024-06-30 16:29:04 -0300

    Changing way to reference technologies

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,26 @@ import { Button, Card, CardBody, CardHeader, Image } from "@nextui-org/react";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
+interface InnerDict {
+  [key: string]: string;
+}
+type AllTechs = {
+  [key: number]: InnerDict;
+};
+
 export default function Projects() {
+  const allTechnologies: AllTechs = {
+    1: { name: "python", src: "images/python-original.svg" },
+    2: { name: "django", src: "images/django-plain.svg" },
+    3: { name: "javascript", src: "images/javascript-original.svg" },
+    4: { name: "typescript", src: "images/typescript-original.svg" },
+    5: { name: "react", src: "images/react-original.svg" },
+    6: { name: "nextjs", src: "images/nextjs-original.svg" },
+    7: { name: "jquery", src: "images/jquery-original.svg" },
+    8: { name: "dart", src: "images/dart-original.svg" },
+    9: { name: "flutter", src: "images/flutter-original.svg" },
+    10: { name: "docker", src: "images/docker-original.svg" },
+  };
   const allProjects = [
     {
       name: "Barbershop",
@@ -10,17 +29,14 @@ export default function Projects() {
       description:
         "A barbershop website where the user can book an appointment and see a real time queue",
       date: "Oct 2021",
-      technologies: [
-        { name: "django", src: "images/django-plain.svg" },
-        { name: "javascript", src: "images/javascript-original.svg" },
-      ],
+      technologies: [1, 2, 3],
     },
     {
       name: "Birthday Reminder",
       imgSrc: "images/projects/birthday-reminder1.png",
       description: "A simple app to remind birthdays via push notifications",
       date: "May 2024",
-      technologies: [{ name: "flutter", src: "images/flutter-original.svg" }],
+      technologies: [8, 9],
     },
     {
       name: "Portfolio",
@@ -28,55 +44,50 @@ export default function Projects() {
       description:
         "The portfolio website you're accessing that showcases my hard work",
       date: "Oct 2021",
-      technologies: [{ name: "nextjs", src: "images/nextjs-original.svg" }],
+      technologies: [4, 5, 6],
     },
   ];
   return (
     <main className="max-w-[1024px] mx-auto">
-      <section
-        id="highlighted-project-section"
-        className="highlighted-project-section"
-      >
-        <h1 className="text-xl mb-8 font-bold">These are all my projects</h1>
+      <h1 className="text-xl mb-8 font-bold">These are all my projects</h1>
 
-        <div className="flex gap-4 mb-8">
-          {allProjects.map((p, i) => (
-            <Card key={i} className="flex-grow basis-0 p-2">
-              <CardHeader className="flex-col items-start pb-0">
-                <h4 className="font-bold text-large">{p.name}</h4>
-                <div className="flex justify-between w-full">
-                  <small className="text-default-500">{p.date}</small>
+      <div className="flex gap-4 mb-8">
+        {allProjects.map((p, i) => (
+          <Card key={i} className="flex-grow basis-0 p-2">
+            <CardHeader className="flex-col items-start pb-0">
+              <h4 className="font-bold text-large">{p.name}</h4>
+              <div className="flex justify-between w-full">
+                <small className="text-default-500">{p.date}</small>
 
-                  <div className="flex justify-center gap-1">
-                    {p.technologies.map((tech, i) => (
-                      <Image
-                        key={i}
-                        src={tech.src}
-                        width={20}
-                        alt="Technology used"
-                        title={tech.name}
-                      />
-                    ))}
-                  </div>
+                <div className="flex justify-center gap-1">
+                  {p.technologies.map((tech, i) => (
+                    <Image
+                      key={i}
+                      src={allTechnologies[tech].src}
+                      width={20}
+                      alt="Technology used"
+                      title={allTechnologies[tech].name}
+                    />
+                  ))}
                 </div>
-                <p className="text-sm">{p.description}</p>
-              </CardHeader>
-              <CardBody className="overflow-visible">
-                <Image alt="Card image" src={p.imgSrc} width={350} isZoomed />
-              </CardBody>
-            </Card>
-          ))}
-        </div>
+              </div>
+              <p className="text-sm">{p.description}</p>
+            </CardHeader>
+            <CardBody className="overflow-visible">
+              <Image alt="Card image" src={p.imgSrc} width={350} isZoomed />
+            </CardBody>
+          </Card>
+        ))}
+      </div>
 
-        <div className="flex justify-center">
-          <Link href="/projects">
-            <Button className="bg-black text-white">
-              See all
-              <ArrowRight />
-            </Button>
-          </Link>
-        </div>
-      </section>
+      <div className="flex justify-center">
+        <Link href="/projects">
+          <Button className="bg-black text-white">
+            See all
+            <ArrowRight />
+          </Button>
+        </Link>
+      </div>
     </main>
   );
 }
