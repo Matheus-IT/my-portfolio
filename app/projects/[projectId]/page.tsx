23a9c4bb8e255af60a759da2411b09bf14ca7cce
matheus <matheuscosta.tech@gmail.com>
2024-07-07 12:11:59 -0300

    Basic see more section for the details page

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -1,18 +1,47 @@
 import { Button } from "@nextui-org/react";
-import { ArrowLeft } from "lucide-react";
+import {
+  ArrowLeft,
+  GithubIcon,
+  Globe,
+  LucideArrowUpRightFromSquare,
+} from "lucide-react";
 import Link from "next/link";
 
 export default function Project({ params }: { params: { projectId: number } }) {
   const projectId = params.projectId;
   return (
     <main className="max-w-[1024px] mx-auto pt-8">
-      <Link href="/projects/">
-        <Button className="bg-black text-white">
-          <ArrowLeft />
-        </Button>
-      </Link>
+      <div className="project-section">
+        <Link href="/projects/">
+          <Button className="bg-black text-white">
+            <ArrowLeft />
+          </Button>
+        </Link>
 
-      <h1>Details page {projectId}</h1>
+        <h1>Details page {projectId}</h1>
+      </div>
+
+      <div className="see-more-section flex flex-col gap-2 p-4 rounded-lg w-fit">
+        <span className="text-lg mb-1 font-bold">
+          Take a look at this project
+        </span>
+
+        <Link href="/projects/" className="m-auto">
+          <Button className="bg-black text-white flex justify-between w-[170px]">
+            <Globe />
+            <span>Live demo</span>
+            <LucideArrowUpRightFromSquare />
+          </Button>
+        </Link>
+
+        <Link href="/projects/" className="m-auto">
+          <Button className="bg-black text-white flex justify-between w-[170px]">
+            <GithubIcon />
+            <span>Code</span>
+            <LucideArrowUpRightFromSquare />
+          </Button>
+        </Link>
+      </div>
     </main>
   );
 }
