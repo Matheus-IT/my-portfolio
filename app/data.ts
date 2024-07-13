8959c95f1ca494621b13cc3a95ee867b5fe9b39c
matheus <matheuscosta.tech@gmail.com>
2024-07-12 22:45:24 -0300

    Detailed description and access to repo

--- app/data.ts
+++ app/data.ts
@@ -18,13 +18,33 @@ export const allTechnologies: AllTechs = {
   10: { name: "docker", src: "images/docker-original.svg" },
 };
 
+const githubUsername = "Matheus-IT";
 export const allProjects = [
   {
     id: 1,
     name: "Barbershop",
     imgSrc: "images/projects/barbershop1.png",
+    liveDemoLink: "",
+    repositoryLink: `https://github.com/${githubUsername}/capstone`,
     description:
       "A barbershop website where the user can book an appointment and see a real time queue",
+    detailedDescription: `
+      <p class="text-base mt-2">Main Functionalities:</p>
+
+      <ul class="list-disc list-inside">
+        <li><strong>Showcase</strong> products and services of a barbershop/hair salon</li>
+        <li>User authentication for <strong>booking appointments</strong> in advance</li>
+        <li><strong>Real-time queue</strong> to inform customers of the best times to visit</li>
+      </ul>
+
+      <p class="text-base text-justify mt-2">
+        Using the knowledge acquired throughout the course <em>CS50</em>, the project aimed to create a <strong>responsive website</strong> for a barbershop/hair salon. The focus was on enhancing the user interface and experience for both mobile and desktop users.
+      </p>
+
+      <p class="text-base text-justify mt-2">
+        A good user experience in a barbershop/hair salon goes beyond just having a nice website. From personal experience, a significant amount of time is often spent waiting for a haircut. To address this, a <strong>real-time queue system</strong> was developed, which clients can access and is managed by the hairstylist. This system allows busy individuals to check the current wait time and choose the most convenient time to visit the salon.
+      </p>
+    `,
     date: "Oct 2021",
     technologies: [1, 2, 3],
   },
@@ -32,17 +52,60 @@ export const allProjects = [
     id: 2,
     name: "Birthday Reminder",
     imgSrc: "images/projects/birthday-reminder1.png",
-    description: "A simple app to remind birthdays via push notifications",
+    liveDemoLink: "",
+    repositoryLink: `https://github.com/${githubUsername}/birthday_app`,
+    description:
+      "A mobile application to remind birthdays via push notifications",
+    detailedDescription: `
+      <p class="text-base mt-2">Main Functionalities:</p>
+
+      <ul class="list-disc list-inside">
+        <li><strong>User authentication</strong> for cataloging a list of birthdays to receive notifications</li>
+        <li>Birthday notifications via <strong>push notifications</strong></li>
+        <li>Birthday notifications via <strong>email</strong></li>
+      </ul>
+
+      <p class="text-base text-justify mt-2">
+        Using the knowledge acquired in college, I developed this app to <strong>help my church</strong> by reminding the birthdays of members.
+      </p>
+
+      <p class="text-base text-justify mt-2">
+        The application is currently in <strong>successful and active use</strong>, and marked my first experience as the product manager of my own application, allowing me to communicate directly with the client.
+      </p>
+
+      <p class="text-base text-justify mt-2">
+        The solution comprises a mobile app built with <em>Flutter</em> and an API designed with <em>Django REST framework</em>.
+      </p>
+    `,
     date: "May 2024",
-    technologies: [8, 9],
+    technologies: [1, 2, 8, 9],
   },
   {
     id: 3,
     name: "Portfolio",
     imgSrc: "images/projects/portfolio1.png",
+    liveDemoLink: "",
+    repositoryLink: `https://github.com/${githubUsername}/my-portfolio`,
     description:
       "The portfolio website you're accessing that showcases my hard work",
-    date: "Oct 2021",
+    detailedDescription: `
+      <p class="text-base mt-2">Main Functionalities:</p>
+
+      <ul class="list-disc list-inside">
+        <li>Main page that <strong>showcases my experience</strong>, technologies I work with, main side projects I built, and a contact section</li>
+        <li>Overview of <strong>all interesting projects</strong> I built to learn and apply my knowledge</li>
+        <li><strong>Detailed page</strong> to learn more about a specific project, access the repository, and see a live demo</li>
+      </ul>
+
+      <p class="text-base text-justify mt-2">
+        My portfolio was built strategically to present who I am, what I do, and what I believe in terms of software development.
+      </p>
+
+      <p class="text-base text-justify mt-2">
+        For technologies, I used <em>Next.js</em> with <em>TypeScript</em> and <em>Tailwind CSS</em>.
+      </p>
+    `,
+    date: "Jul 2024",
     technologies: [4, 5, 6],
   },
 ];
