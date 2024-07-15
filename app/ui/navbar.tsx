a656c83ccc78cd29b6d6b44cddccd8511299a740
matheus <matheuscosta.tech@gmail.com>
2024-07-15 12:04:36 -0300

    No need to set active on navbar

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import {
-  Avatar,
   Navbar as NextUiNavbar,
-  NavbarBrand,
   NavbarContent,
   NavbarItem,
 } from "@nextui-org/react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-  const pathName = usePathname();
   const links = [
     { name: "About", href: "/" },
     { name: "Main Projects", href: "#highlighted-project-section" },
@@ -25,7 +21,6 @@ export default function Navbar() {
           return (
             <NavbarItem
               key={l.name}
-              isActive={l.href == pathName}
               className="p-2 rounded hover:bg-neutral-800 transition-background duration-200"
             >
               <Link href={l.href}>{l.name}</Link>
