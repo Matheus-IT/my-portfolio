fb3cdedf17e4d8a34c606a64523b5af3bf1f7906
matheus <matheuscosta.tech@gmail.com>
2024-07-19 00:00:39 -0300

    Fix margin on details page for mobile

--- app/projects/[projectId]/page.tsx
+++ app/projects/[projectId]/page.tsx
@@ -17,7 +17,7 @@ export default function Project({ params }: { params: { projectId: number } }) {
   return (
     <>
       <main className=" max-w-[1024px] mx-auto pt-8">
-        <div className="flex gap-4 items-start m-4 mb-16 max-md:flex-col">
+        <div className="flex gap-4 items-start mb-16 max-md:flex-col max-sm:m-2 max-xl:m-4">
           <div className="project-section relative rounded-lg">
             <Link className="absolute left-2 top-2 z-20" href="/projects/">
               <Button className="bg-black text-white border border-white border-solid">
@@ -56,7 +56,7 @@ export default function Project({ params }: { params: { projectId: number } }) {
             </div>
           </div>
 
-          <div className="see-more-section flex flex-col gap-2 p-4 rounded-lg min-w-[fit-content] max-md:w-[100%] max-md:text-center">
+          <div className="see-more-section flex flex-col gap-2 p-4 rounded-lg max-md:w-full min-w-[fit-content] max-md:text-center">
             <span className="text-lg mb-1 font-bold">
               Take a look at this project
             </span>
