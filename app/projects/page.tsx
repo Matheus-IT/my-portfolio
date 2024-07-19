c0e38f9eb0217d75a309707e461bf8981290205c
matheus <matheuscosta.tech@gmail.com>
2024-07-18 23:02:29 -0300

    Making the all projects page responsive

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -15,17 +15,17 @@ import ProjectCard from "../ui/projectCard";
 export default function Projects() {
   return (
     <main className="max-w-[1024px] mx-auto pt-8">
-      <Link href="/">
+      <Link href="/" className="max-sm:m-2 max-xl:m-4">
         <Button className="bg-black text-white">
           <ArrowLeft />
         </Button>
       </Link>
 
-      <h1 className="text-xl text-center mb-8 font-bold">
+      <h1 className="text-xl text-center mb-8 max-sm:m-2 max-sm:my-4 max-xl:m-4 font-bold">
         These are all my projects
       </h1>
 
-      <div className="grid grid-cols-3 gap-4 mb-16">
+      <div className="grid grid-cols-3 max-sm:grid-cols-1 max-md:grid-cols-2 gap-4 mb-16 max-sm:m-2 max-xl:m-4">
         {allProjects.map((p, i) => (
           <ProjectCard key={p.id} project={p} />
         ))}
